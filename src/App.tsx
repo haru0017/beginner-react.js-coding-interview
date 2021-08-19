0366e1b6196316aa6fe8e4d4ecd830cea8c09d22
haru <61040486+haru0017@users.noreply.github.com>
2021-08-19 11:56:30 +0900

    create counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <p>{counter}</p>
+      <button onClick={() => setCounter(counter + 1)}>Increase counter</button>
     </div>
   );
 }
