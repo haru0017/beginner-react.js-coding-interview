5561b0550db228be30a49c78843f1b0c0be1cce8
haru <61040486+haru0017@users.noreply.github.com>
2021-08-19 19:22:01 +0900

    fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
 import "./styles.css";
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const FetchRandomData = () => {
+  return axios
+    .get("https://randomuser.me/api")
+    .then((data) => {
+      console.log(data);
+      return JSON.stringify(data, null, 2);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 export default function App() {
   const [counter, setCounter] = useState(0);
+  const [randomUserDataJson, setRandomUserDataJson] = useState("");
+
+  useEffect(() => {
+    FetchRandomData().then((randomData) => {
+      setRandomUserDataJson(randomData || "No user data found.");
+    });
+  }, []);
 
   return (
     <div className="App">
@@ -10,6 +30,7 @@ export default function App() {
       <h2>Start editing to see some magic happen!</h2>
       <p>{counter}</p>
       <button onClick={() => setCounter(counter + 1)}>Increase counter</button>
+      <pre>{randomUserDataJson}</pre>
     </div>
   );
 }
