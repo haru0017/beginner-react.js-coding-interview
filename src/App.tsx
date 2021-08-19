488f615609448fb3b357f22ed980123624753a0c
haru <61040486+haru0017@users.noreply.github.com>
2021-08-19 23:00:16 +0900

    show name and thumbnail

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,51 @@ import "./styles.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+interface UserName {
+  first: string;
+  last: string;
+  title: string;
+}
+
+interface UserPicture {
+  thumbnail: string;
+}
+
+interface Userinfo {
+  name: UserName;
+  picture: UserPicture;
+}
+
 const FetchRandomData = () => {
   return axios
     .get("https://randomuser.me/api")
-    .then((data) => {
+    .then(({ data }) => {
       console.log(data);
-      return JSON.stringify(data, null, 2);
+      return data;
     })
     .catch((err) => {
       console.log(err);
     });
 };
 
+const getFullUserName = (userinfo: Userinfo) => {
+  const {
+    name: { first, last }
+  } = userinfo;
+  return `${first} ${last}`;
+};
+
 export default function App() {
   const [counter, setCounter] = useState(0);
+  const [userInfos, setUserinfos] = useState<any>([]);
   const [randomUserDataJson, setRandomUserDataJson] = useState("");
 
   useEffect(() => {
     FetchRandomData().then((randomData) => {
-      setRandomUserDataJson(randomData || "No user data found.");
+      setRandomUserDataJson(
+        JSON.stringify(randomData, null, 2) || "No user data found."
+      );
+      setUserinfos(randomData.results);
     });
   }, []);
 
@@ -30,6 +56,12 @@ export default function App() {
       <h2>Start editing to see some magic happen!</h2>
       <p>{counter}</p>
       <button onClick={() => setCounter(counter + 1)}>Increase counter</button>
+      {userInfos.map((userinfo: Userinfo, idx: number) => (
+        <div key={idx}>
+          <p>{getFullUserName(userinfo)}</p>
+          <img src={userinfo.picture.thumbnail} alt="" />
+        </div>
+      ))}
       <pre>{randomUserDataJson}</pre>
     </div>
   );
